fd0a4364bb50fa96bc52f2b4e1ab257edd146563
harveyff <harvey@financingfeature.com>
2023-03-19 01:01:49 +0800

    test

--- apps/indexer/lib/indexer/block/fetcher.ex
+++ apps/indexer/lib/indexer/block/fetcher.ex
@@ -55,8 +55,6 @@ defmodule Indexer.Block.Fetcher do
               %{
                 address_hash_to_fetched_balance_block_number: address_hash_to_fetched_balance_block_number,
                 addresses: Import.Runner.options(),
-                #address_coin_balances: Import.Runner.options(),
-                #address_coin_balances_daily: Import.Runner.options(),
                 address_token_balances: Import.Runner.options(),
                 blocks: Import.Runner.options(),
                 block_second_degree_relations: Import.Runner.options(),
@@ -176,6 +174,7 @@ defmodule Indexer.Block.Fetcher do
              state,
              %{
                addresses: %{params: addresses},
+               address_coin_balances: %{params: []},
                address_token_balances: %{params: address_token_balances},
                blocks: %{params: blocks},
                block_second_degree_relations: %{params: block_second_degree_relations_params},
