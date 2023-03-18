08d3abcb4e2477d55e5def765a9194358548ff97
harveyff <harvey@financingfeature.com>
2023-03-19 01:19:45 +0800

    test

--- apps/indexer/lib/indexer/block/realtime/fetcher.ex
+++ apps/indexer/lib/indexer/block/realtime/fetcher.ex
@@ -204,16 +204,27 @@ defmodule Indexer.Block.Realtime.Fetcher do
           {:ok,
            %{
              addresses_params: balances_addresses_params,
-             balances_params: balances_params
+             balances_params: balances_params,
+             balances_daily_params: balances_daily_params
            }}} <-
+           {:balances,
+            balances(block_fetcher, %{
+              address_hash_to_block_number: address_hash_to_block_number,
+              addresses_params: addresses_params,
+              balances_params: address_coin_balances_params,
+              balances_daily_params: address_coin_balances_daily_params
+            })},
+         {block_reward_errors, chain_import_block_rewards} = Map.pop(block_rewards, :errors),
          chain_import_options =
            options
            |> Map.drop(@import_options)
            |> put_in([:addresses, :params], balances_addresses_params)
-           |> put_in([:blocks, :params, Access.all(), :consensus], true),
+           |> put_in([:blocks, :params, Access.all(), :consensus], true)
+           |> put_in([:block_rewards], chain_import_block_rewards),
          {:import, {:ok, imported} = ok} <- {:import, Chain.import(chain_import_options)} do
       async_import_remaining_block_data(
-        imported
+        imported,
+        %{block_rewards: %{errors: block_reward_errors}}
       )
 
       Accounts.drop(imported[:addresses])
